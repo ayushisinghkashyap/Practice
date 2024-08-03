dfb166eb8063494dc3405165bb58e22d57173308
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-08-03 19:56:23 +0530

    Create area_circle.java

---
+++ area_circle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * area_circle
+ */
+public class area_circle {
+public static void main(String[] args) {
+
+    Scanner sc =new Scanner(System.in);
+    int r = sc.nextInt();
+    int area = 10*r*r;
+    System.out.print("Area of Circle : ");
+    System.out.print(area);
+}
+}
